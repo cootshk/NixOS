a5179a721d2f2a64ae9649dc42ec8c6cb0275634
Henry <cootshk@duck.com>
2024-10-26 12:26:57 -0500

    virtualization

--- hosts/common.nix
+++ hosts/common.nix
@@ -286,9 +286,25 @@
     morewaita-icon-theme
   ];
 
-  #virtualisation.libvirtd.enable = true;
-  #virtualisation.spiceUSBRedirection.enable = true;
-  #programs.virt-manager.enable = true;
+  # virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
+
+  virtualisation.spiceUSBRedirection.enable = true;
+  programs.virt-manager.enable = true;
 
   # Enable sddm login manager
   services.displayManager.sddm = {
