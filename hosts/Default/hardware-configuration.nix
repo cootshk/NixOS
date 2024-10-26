02149febf9c5769287ea368e3f68a46ece7a7232
Henry <cootshk@duck.com>
2024-10-25 22:51:17 -0500

    move C:\ to a ext4 drive

--- hosts/Default/hardware-configuration.nix
+++ hosts/Default/hardware-configuration.nix
@@ -29,9 +29,9 @@
       options = ["nofail" "uid=1000" "gid=100" "dmask=007" "fmask=117" "user" "u+rwx" "g+rwx" "o+rwx" "users" "a+rwx" "exec" "umask=000"];
     };
   fileSystems."/mnt/c" =
-    { device = "/dev/disk/by-uuid/6EA4E657A4E62177";
-      fsType = "ntfs-3g";
-      options = ["nofail" "uid=1000" "gid=100" "dmask=007" "fmask=117" "user" "u+rwx" "g+rwx" "o+rwx" "users" "a+rwx" "exec" "umask=000"];
+    { device = "/dev/disk/by-uuid/a3d31b70-c6be-43ee-a470-7a77d471ed45";
+      fsType = "ext4";
+      # options = ["nofail" "uid=1000" "gid=100" "dmask=007" "fmask=117" "user" "u+rwx" "g+rwx" "o+rwx" "users" "a+rwx" "exec" "umask=000"];
     };
 
   swapDevices = [ ];
