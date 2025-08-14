81ab0c5e0eccb4c82b4c1c0132f46f7531eb4707
Henry <cootshk@duck.com>
2025-08-14 14:14:05 -0500

    hypr wallpaper fix

--- modules/desktop/hyprland/hypr/scripts/wallpaper.sh
+++ modules/desktop/hyprland/hypr/scripts/wallpaper.sh
@@ -4,5 +4,5 @@ swww query
 if [ $? -eq 1 ] ; then
     swww init
 fi
-swww img ~/.config/hypr/monitor-left.gif --outputs $(xrandr --query | grep HDMI | sed "s/ .*//")
-swww img ~/.config/hypr/monitor-right.png --outputs $(xrandr --query | grep DP | sed "s/ .*//")
+swww img ~/.config/hypr/wallpaper-left.gif --outputs $(xrandr --query | grep HDMI | sed "s/ .*//")
+swww img ~/.config/hypr/wallpaper-right.png --outputs $(xrandr --query | grep DP | sed "s/ .*//")
